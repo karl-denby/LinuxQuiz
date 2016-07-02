f50bfa8f92b30cfe63689ad3af26a34bc5616285
Karl Denby <karl.denby@gmail.com>
2016-07-02 16:36:22 +0100

    layout v1, beginning of answer checking code

--- app/src/main/java/com/example/android/linuxquiz/MainActivity.java
+++ app/src/main/java/com/example/android/linuxquiz/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.android.linuxquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkAnswers(View view) {
+        int correct_answers = 0;
+
+        // Question 1
+        String q1_answer = getString(R.string.answer1);
+        EditText q1_input = (EditText) findViewById(R.id.answer1);
+        if (q1_input.getText().toString() == q1_answer) {
+            correct_answers += 1;
+        }
+
+        // Question 2
+        RadioButton q2_input = (RadioButton) findViewById(R.id.q2_radio_b);
+        if (q2_input.isChecked()) {
+            correct_answers += 1;
+        }
+
+
+        // Output the results
+        Toast.makeText(this, "You scored " + correct_answers + "/10", Toast.LENGTH_LONG).show();
+    }
+
 }
