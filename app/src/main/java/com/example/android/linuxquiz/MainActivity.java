f06c44c4e37d7674b8c883fdfc46ffcd319b0553
Karl Denby <karl.denby@gmail.com>
2016-07-02 17:07:30 +0100

    checking code functional

--- app/src/main/java/com/example/android/linuxquiz/MainActivity.java
+++ app/src/main/java/com/example/android/linuxquiz/MainActivity.java
@@ -19,19 +19,48 @@ public class MainActivity extends AppCompatActivity {
     public void checkAnswers(View view) {
         int correct_answers = 0;
 
-        // Question 1
+        // Question 1 - Edit Text
         String q1_answer = getString(R.string.answer1);
         EditText q1_input = (EditText) findViewById(R.id.answer1);
-        if (q1_input.getText().toString() == q1_answer) {
+        if (q1_input.getText().toString().equals(q1_answer)) {
             correct_answers += 1;
         }
 
-        // Question 2
+        // Question 2 - RadioButton
         RadioButton q2_input = (RadioButton) findViewById(R.id.q2_radio_b);
         if (q2_input.isChecked()) {
             correct_answers += 1;
         }
 
+        // Questions 3 - CheckBoxes
+        CheckBox q3_input_a = (CheckBox) findViewById(R.id.q3_checkbox_a);
+        CheckBox q3_input_b = (CheckBox) findViewById(R.id.q3_checkbox_b);
+        CheckBox q3_input_c = (CheckBox) findViewById(R.id.q3_checkbox_c);
+
+        if (!q3_input_a.isChecked() && q3_input_b.isChecked() && q3_input_c.isChecked()) {
+            correct_answers += 1;
+        }
+
+        // Question 4
+        correct_answers += 1;
+
+        // Question 5
+        correct_answers += 1;
+
+        // Question 6
+        correct_answers += 1;
+
+        // Question 7
+        correct_answers += 1;
+
+        // Question 8
+        correct_answers += 1;
+
+        // Question 9
+        correct_answers += 1;
+
+        // Question 10
+        correct_answers += 1;
 
         // Output the results
         Toast.makeText(this, "You scored " + correct_answers + "/10", Toast.LENGTH_LONG).show();
